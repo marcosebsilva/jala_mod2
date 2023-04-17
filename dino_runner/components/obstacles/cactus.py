b27f9955b39068bc72811cd92e81c3b45d63a04d
Marcos Silva <dusdoom.dev@gmail.com>
2023-04-17 02:55:17 -0300

    refactor: updated Cactus to allow different y positions

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,10 +1,12 @@
-import pygame
 import random
 
 from dino_runner.components.obstacles.obstacle import Obstacle
 
 class Cactus(Obstacle):
-    def __init__(self, image):
-        self.type = random.randint(0, 2)
+    def __init__(self, image, rect_y = 325):
+        self.type = random.randint(0, len(image) - 1)
         super().__init__(image, self.type)
-        self.rect.y = 325
+        # this allows us to have different sized cactus
+        # the bad thing is that we have to directly set a Y value.
+        # i should probably make a better way to do this
+        self.rect.y = rect_y
